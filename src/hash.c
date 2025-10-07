5cde8948f2413ab6b80650fa550959b23e5f96be
akto <valentin.colindre@gmail.com>
2025-10-07 19:27:03 +0200

    Add: documentation fonction reduce

--- src/hash.c
+++ src/hash.c
@@ -11,7 +11,11 @@ pwhash target_hash_function (const void *data) {
 }
 
 // fonction de mélange rapide
-static inline uint64_t fast_mix(uint64_t x) 
+//récupérée de MurmurHash3 pour essayer
+//de produire des changements importants
+// avec efforts minimes tout en étant facile à inverser.
+//Source de la méthode : https://github.com/aappleby/smhasher/blob/master/src/MurmurHash3.cpp#L81
+uint64_t avalanche(uint64_t x)
 {
     x ^= x >> 33;
     x *= 0xff51afd7ed558ccd;
@@ -22,15 +26,24 @@ static inline uint64_t fast_mix(uint64_t x)
 // fonction de réduction
 void reduction(uint64_t hash, int variation, int allowed_chars_length, char* allowed_chars, char* pass) 
 {
+    //On mélange déjà un peu le hash d'origine en avance via l'avalanche
+    // auquel on fournit le hash mélangé via xor au nombre d'or * variation pour
+    // répartir les changements sur les bits de façon équiloibrée
+    uint64_t state = avalanche(hash ^ ((uint64_t)variation * 0x9e3779b97f4a7c15));
 
-    uint64_t state = fast_mix(hash ^ ((uint64_t)variation * 0x9e3779b97f4a7c15));
-    
-    // Générer un mot de passe de M caractères
+
     for (int i = 0; i < M; i++) 
     {
+        //On récupère un char de l'alphabet avec le modulo de la taille de l'alphabet et l'état actuel
+        //du hash altéré
         pass[i] = allowed_chars[state % allowed_chars_length];
-        state = fast_mix(state + i + variation);
+        //On altère de nouveau le hash pour la suite du mot de passe
+        //Cette fois sans le nombre d'or, uniquement avec la position du char et la variation
+        //de la fonction de réduc
+        state = avalanche(state + i + variation);
     }
+
+    //Ajoute \0 pour finir la chaine de char
     pass[M] = '\0';
 }
 
